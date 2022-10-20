cd834d2611c1bd78f3664fce8993122e0983eba2
Joy Chandra Mollik <xoycmollik913@gmail.com>
2022-10-20 12:36:35 +0600

    opened development branch

--- ulearn-react-client-side/src/App.js
+++ ulearn-react-client-side/src/App.js
@@ -3,14 +3,21 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import CourseList from './pages/CourseList/CourseList';
 import AdminDashboardHome from './dashboards/AdminDashboard/AdminDashboardHome/AdminDashboardHome';
+import Auth from './pages/Auth/Auth';
+import Login from './components/Auth/Login/Login';
+import Register from './components/Auth/Register/Register';
 
 function App() {
   return (
 		<>
 			<Routes>
 				<Route index element={ <Home /> } />
-				<Route path='/course-list' element={ <CourseList /> } />
-				<Route path='/admin/dashboard' element= { <AdminDashboardHome /> } />
+				<Route path='course-list' element={ <CourseList /> } />
+				<Route path='auth' element={<Auth />} >
+					<Route path='login' element={<Login />} />
+					<Route path='register' element={<Register />} />
+				</Route>
+				<Route path='admin/dashboard' element= { <AdminDashboardHome /> } />
 			</Routes>
 		</>
   );
