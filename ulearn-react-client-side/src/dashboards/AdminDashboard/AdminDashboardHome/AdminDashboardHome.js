ab82cf56efca6ff7c9fbcc911e60f226c0189499
Joy Chandra Mollik <xoycmollik913@gmail.com>
2022-10-29 22:49:36 +0600

    added dashboard header

--- ulearn-react-client-side/src/dashboards/AdminDashboard/AdminDashboardHome/AdminDashboardHome.js
+++ ulearn-react-client-side/src/dashboards/AdminDashboard/AdminDashboardHome/AdminDashboardHome.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Outlet, Route, Routes } from 'react-router-dom';
-import image from '../../../images/dashboard_background.svg';
-import DashboardSidebar from '../../DashboardLayout/DashboardSidebar/DashboardSidebar';
 import { MdOutlineDashboard } from 'react-icons/md';
+// components
+import DashboardSidebar from '../../DashboardLayout/DashboardSidebar/DashboardSidebar';
+import DashboardHeader from '../../DashboardLayout/DashboardHeader/DashboardHeader';
 
 const adminDashboardLinks = [
 	{
@@ -19,17 +20,16 @@ const adminDashboardLinks = [
 
 const AdminDashboardHome = () => {
 	return (
-		<div
-			className='min-h-screen'
-		>
+		<div className='min-h-screen'>
 			<div className='admin-dash-container grid grid-cols-12 gap-8 min-h-screen'>
-				{/* dashboard left panel */}
-				<div className='col-span-2'>
+				{/*****--------------dashboard left panel---------------*****/}
+				<div className='col-span-2 py-5'>
 					<DashboardSidebar />
 				</div>
 
-				{/* dashboard right panel */}
-				<div className='col-span-10 bg-light'>
+				{/*****--------------dashboard right panel---------------*****/}
+				<div className='col-span-10 bg-light p-5'>
+					<DashboardHeader />
 					<Outlet />
 				</div>
 			</div>
--- ulearn-react-client-side/src/dashboards/DashboardLayout/SideBarLink/SideBarLink.js
+++ ulearn-react-client-side/src/dashboards/DashboardLayout/SideBarLink/SideBarLink.js
@@ -12,7 +12,7 @@ const SideBarLink = React.forwardRef(
 						props.className,
 						'px-4 py-2 rounded-lg flex items-center space-x-2 text-base hover:bg-light hover:text-font1',
 						isActive
-							? ' text-primary border-l-[4px] border-primary bg-gradient-to-r from-light to-transparent'
+							? ' text-primary border-l-[4px] border-primary bg-gradient-to-r from-light via-transparent to-transparent'
 							: 'text-font1',
 					]
 						.filter(Boolean)
