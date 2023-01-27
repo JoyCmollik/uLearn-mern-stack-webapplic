292899af8abc9fcf79434292c27659be788b19f5
Joy Chandra Mollik <xoycmollik913@gmail.com>
2023-01-27 21:31:52 +0600

    fixing cookies

--- ulearn-node-server-side/utils/jwt.js
+++ ulearn-node-server-side/utils/jwt.js
@@ -20,7 +20,7 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === 'production',
 		signed: true,
-
+		sameSite: 'none',
 		expires: new Date(Date.now() + oneDay),
 	});
 
@@ -28,7 +28,7 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === 'production',
 		signed: true,
-
+		sameSite: 'none',
 		expires: new Date(Date.now() + longerExp),
 	});
 };
