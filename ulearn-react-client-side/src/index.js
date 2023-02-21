ce996b3b207ef17dda92caeb6b20c5e01606aa38
Joy Chandra Mollik <xoycmollik913@gmail.com>
2023-02-21 11:30:54 +0600

    fixed env file bugs

--- ulearn-react-client-side/src/index.js
+++ ulearn-react-client-side/src/index.js
@@ -7,8 +7,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AuthProvider from './contexts/AuthProvider';
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.BASEURL;
-axios.defaults.withCredentials = process.env.PRODUCTION;
+axios.defaults.baseURL = process.env.REACT_APP_BASEURL;
+axios.defaults.withCredentials = process.env.REACT_APP_PRODUCTION;
+
+console.log('from index', process.env.REACT_APP_PRODUCTION);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
